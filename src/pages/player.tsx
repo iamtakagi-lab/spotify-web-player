4509351b227637c201a33ed5a4d75746336f9619
takagi <iam.takagi.dev@gmail.com>
2021-11-20 17:30:06 +0900

    Fix deviceName

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -20,7 +20,7 @@ const Player: React.VFC<Props> = ({ token }) => {
     [token.access_token],
   );
 
-  const [deviceName, setDeviceName] = useState("Spotify Player on Next.js");
+  const [deviceName, setDeviceName] = useState("Spotify Web Player NowPlaying");
 
   return (
     <WebPlaybackSDK
